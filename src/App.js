d539c93c4d08efcf0b9f889b59dbeedf7890d3cd
baaaam771 <jeongiun@naver.com>
2022-03-06 19:44:00 +0900

    add city

--- src/App.js
+++ src/App.js
@@ -19,7 +19,9 @@ function App() {
         첫 게시글입니다.
       </div>
       <WorldClock city={'서울'} time={10} />
-      <WorldClock city={'부산'} time={10} />
+      <WorldClock city={'베이징'} time={9} />
+      <WorldClock city={'시드니'} time={12} />
+      <WorldClock city={'LA'} time={17} />
     </div>
   );
 }
