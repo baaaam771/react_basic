f0858e29c70ea0b671c2e76065ad86b3f4adf6ba
baaaam771 <jeongiun@naver.com>
2022-03-06 22:59:38 +0900

    event handling

--- src/App.js
+++ src/App.js
@@ -52,30 +52,71 @@ class WorldClock extends Component {
 }
 
 
-function App() {
-  const cityTimeData = [
-    ['서울', 10],
-    ['베이징', 9],
-    ['시드니', 12],
-    ['LA', 17]
-  ]
+// function App() {
+//   const cityTimeData = [
+//     ['서울', 10],
+//     ['베이징', 9],
+//     ['시드니', 12],
+//     ['LA', 17]
+//   ]
 
-  const WorldClockList = cityTimeData.map((citytime, index)=>
-    <WorldClock city = {citytime[0]} time = {citytime[1]} key = {index} />
-  )
-  return (
-    <div className="App">
-      <h1 className={'myStyle'}>안녕하세요</h1>
-      <div className={'post'}>
-        첫 게시글입니다.
+//   const WorldClockList = cityTimeData.map((citytime, index)=>
+//     <WorldClock city = {citytime[0]} time = {citytime[1]} key = {index} />
+//   )
+//   return (
+//     <div className="App">
+//       <h1 className={'myStyle'}>안녕하세요</h1>
+//       <div className={'post'}>
+//         첫 게시글입니다.
+//       </div>
+//       {/* <WorldClock city={'서울'} time={10} />
+//       <WorldClock city={'베이징'} time={9} />
+//       <WorldClock city={'시드니'} time={12} />
+//       <WorldClock city={'LA'} time={17} /> */}
+//       {WorldClockList}
+//     </div>
+//   );
+// }
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.cityTimeData = [
+      ['서울', 10],
+      ['베이징', 9],
+      ['시드니', 12],
+      ['LA', 17]
+    ]
+    this.state = {
+      content: ''
+    }
+
+  }
+
+  handlingChange = (event) => {
+    this.setState({content: event.target.value})
+  }
+  
+  render() {
+
+    return (
+      <div className="App">
+        <h1 className={'myStyle'}>안녕하세요</h1>
+        <div className={'post'}>
+          첫 게시글입니다.
+          <br />
+          <textarea value={this.state.content} onChange={this.handlingChange}></textarea>
+        </div>
+        {this.cityTimeData.map((citytime, index)=>
+          <WorldClock city = {citytime[0]} time = {citytime[1]} key = {index} />
+        )}
       </div>
-      {/* <WorldClock city={'서울'} time={10} />
-      <WorldClock city={'베이징'} time={9} />
-      <WorldClock city={'시드니'} time={12} />
-      <WorldClock city={'LA'} time={17} /> */}
-      {WorldClockList}
-    </div>
-  );
+    );
+  }
+ 
+  
 }
 
 export default App;
